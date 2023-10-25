20e535bd109dd1b3eaf8125d9fe4376191660846
Aczino885802 <camunoza@libertadores.edu.co>
2023-10-25 10:50:25 -0500

    Ya está sirviendo la funcion de eliminar el producto de la tabla ;)

--- app/src/main/java/com/example/papeleria/EliminarProducto.java
+++ app/src/main/java/com/example/papeleria/EliminarProducto.java
@@ -14,15 +14,17 @@ import android.widget.Toast;
 import com.example.papeleria.constants.Constantes;
 import com.example.papeleria.entities.Producto;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class EliminarProducto extends AppCompatActivity {
     private EditText nombreProd;
     private Spinner spinnerTipo;
     private ArrayList<Producto> listaProductos = new ArrayList<>();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,21 +36,31 @@ public class EliminarProducto extends AppCompatActivity {
         ArrayAdapter<String> listaOpcionesTipo = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opciones);
         spinnerTipo.setAdapter(listaOpcionesTipo);
 
+        // Cargar la lista de productos desde el archivo (si existe)
+        cargarProductos();
     }
-    public void eliminar (View view) {
-        Producto nuevoProducto = new Producto();
 
-        nuevoProducto.setNombre(nombreProd.getText().toString());
-        nuevoProducto.setTipo(spinnerTipo.getSelectedItem().toString());
+    public void eliminar(View view) {
+        String nombreEliminar = nombreProd.getText().toString();
+        String tipoEliminar = spinnerTipo.getSelectedItem().toString();
 
-        listaProductos.add(nuevoProducto);
+        // Iterar sobre la lista de productos y eliminar el producto con el nombre y tipo especificados
+        Iterator<Producto> iterator = listaProductos.iterator();
+        while (iterator.hasNext()) {
+            Producto producto = iterator.next();
+            if (producto.getNombre().equals(nombreEliminar) && producto.getTipo().equals(tipoEliminar)) {
+                iterator.remove();
+            }
+        }
 
+        // Guardar la lista de productos actualizada
         EliminarProd();
-        Toast.makeText(this, "Producto Eliminado", Toast.LENGTH_SHORT).show();
 
+        Toast.makeText(this, "Producto Eliminado", Toast.LENGTH_SHORT).show();
         Intent mainActivity = new Intent(this, MainActivity.class);
         startActivity(mainActivity);
     }
+
     public void EliminarProd() {
         try {
             FileOutputStream fos = openFileOutput(Constantes.ARCHIVO_CARGA_LOCAL_INVENTARIO, Context.MODE_PRIVATE);
@@ -57,7 +69,17 @@ public class EliminarProducto extends AppCompatActivity {
             oss.close();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
 
+    private void cargarProductos() {
+        try {
+            FileInputStream fis = openFileInput(Constantes.ARCHIVO_CARGA_LOCAL_INVENTARIO);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            listaProductos = (ArrayList<Producto>) ois.readObject();
+            ois.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
